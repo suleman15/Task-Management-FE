482a662f3dda0f3f0fd02455425df7e80f034d89
suleman15 <sulemanahmed2704@gmail.com>
2024-06-17 22:54:37 +0500

    task api route added

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,8 +1,4 @@
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9
-//   .eyJlbWFpbCI6Il90ZXN0aW5nQGdtYWlsLmNvbSIsImlkIjoiNjY3MDYxNjVmZWM5OGVlZmZiODgxNGVhIiwiaWF0IjoxNzE4NjQxMTE2LCJleHAiOjE3MTg4MjExMTZ9
-//   .UCrQiUQN6IoTRay024Z4T_v5SLOFUmdRvUkZZDXoCdc;
 import express from "express";
-import { Response } from "../utils/Response.js";
 import { containToken } from "../middlewares/containToken.middleware.js";
 import { taskSchema } from "../schema/task.schema.js";
 import { validate } from "../middlewares/validation.middlewares.js";
@@ -19,12 +15,12 @@ const router = express.Router();
 router
   .route("/")
   .post(containToken, validate(taskSchema), createTaskController)
-  .get(getTasksController);
+  .get(containToken, getTasksController);
 
 router
   .route("/:id")
-  .get(getTaskByIdController)
-  .put(updateTaskController)
-  .delete(deleteTaskController);
+  .get(containToken, getTaskByIdController)
+  .put(containToken, updateTaskController)
+  .delete(containToken, deleteTaskController);
 
 export { router as TaskRouter };
